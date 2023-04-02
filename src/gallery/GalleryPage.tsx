aa79ed609039e2067c4ce4c41fe4d5280aca0006
gasin <7404299+gasin@users.noreply.github.com>
2023-04-02 16:38:32 +0900

    add images to gallery

--- src/gallery/GalleryPage.tsx
+++ src/gallery/GalleryPage.tsx
@@ -1,11 +1,33 @@
 import { Fragment } from "react";
 
 function GalleryPage() {
+  const imgFiles = [
+    {"id": 0, "name": "kubosan", "path": require("../assets/gallery/kubosan.png"), "description": "久保さんのFA"},
+    {"id": 1, "name": "koshimizusan", "path": require("../assets/gallery/koshimizu.png"), "description": "小清水透さんのFA"},
+    {"id": 2, "name": "orange_nature", "path": require("../assets/gallery/orange_nature.png"), "description": "風景画像模写"},
+    {"id": 3, "name": "chenso", "path": require("../assets/gallery/chenso.png"), "description": "チェンソーマン模写"},
+  ];
+
+  const imgPath = (fileName: string) => (
+    "../assets/gallery/" + fileName
+  );
+
   return (
     <Fragment>
       <h2>Gallery</h2>
       過去に書いた絵。
-      <p>気が向いたら貼る</p>
+      <div className="row">
+        {imgFiles.map((imgFile) => (
+          <div key={imgFile.id} className="cols-sm">
+            <div className="card">
+              <img src={imgFile.path} alt={imgFile.name} />
+              <section className="section dark">
+                {imgFile.description}
+              </section>
+            </div>
+          </div>
+        ))}
+      </div>
     </Fragment>
   )
 }
