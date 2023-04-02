390a7e69fecfd49be851bc8691f4438010a57c95
gasin <7404299+gasin@users.noreply.github.com>
2023-04-02 15:34:27 +0900

    fix indent

--- src/gallery/GalleryPage.tsx
+++ src/gallery/GalleryPage.tsx
@@ -1,13 +1,13 @@
 import { Fragment } from "react";
 
 function GalleryPage() {
-    return (
-        <Fragment>
-            <h2>Gallery</h2>
-            過去に書いた絵。
-            <p>気が向いたら貼る</p>
-        </Fragment>
-    )
+  return (
+    <Fragment>
+      <h2>Gallery</h2>
+      過去に書いた絵。
+      <p>気が向いたら貼る</p>
+    </Fragment>
+  )
 }
 
 export default GalleryPage;
